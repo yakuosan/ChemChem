c8654977a3c947c66283f2546f068c22006d59f6
yakuosan <yakuo9359@gmail.com>
2026-01-29 04:13:48 +0900

    Initial Commit

---
+++ ChemChem/ChemChemApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemChemApp.swift
+//  ChemChem
+//
+//  Created by 阪上八雲 on 2026/01/29.
+//
+
+import SwiftUI
+
+@main
+struct ChemChemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChemChem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChemChem
+//
+//  Created by 阪上八雲 on 2026/01/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
